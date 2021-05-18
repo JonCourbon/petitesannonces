339f8032da959d2cf684989a8cc98365d23dfab4
JonCourbon <joncourbon@gmail.com>
2021-05-18 11:00:52 +0200

    Initialisation

---
+++ README.md
@@ -0,0 +1,4 @@
+#Petites annonces
+
+
+Exemple de site utilisant une base de données et des éléments animés en Js
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Petites annonces</title>
+  <meta name="description" content=""/>
+  
+  
+  
+  <!-- amélioration de la cohérence entre les navigateurs dans le style par défaut des éléments HTML-->
+  <link rel="stylesheet" media="screen" href="css/normalize.css">
+  
+  <!-- utilisation de Boostrap (via un CDN) -->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+  
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+  
+  <!-- style CSS perso-->
+  <link rel="stylesheet" media="screen" href="css/style.css">
+  
+</head>
+<body>
+  
+  
+  <!-- utilisation de Boostrap (via un CDN) -->
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+  
+</body>
+</html>
